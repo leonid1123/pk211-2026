01b1f28b08b8ed8acb13335e2c85977a06938b85
Leo <leonid112@yandex.ru>
2026-02-20 10:31:35 +0300

    new

--- main.py
+++ main.py
@@ -2,16 +2,28 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import random
 
 app = Flask(__name__)
 
+def get_random_int():
+    return random.randrange(0,10)
+
+AI_number = get_random_int()
+
 @app.route("/", methods=['POST','GET'])
 def main_page():
+    global AI_number
     if request.method == 'POST':
-        new_name = request.form['nazvanie']
-        new_price = request.form['price']
-        return render_template('index.html', 
-                                user_new_name=new_name, 
-                                user_new_price=new_price)
-    return render_template('index.html')
+        user_input = int(request.form['num'])
+        if user_input > AI_number:
+            txt = 'Чиселка очень большая'
+        elif user_input < AI_number:
+            txt = 'Чиселка очень маленькая'
+        else:
+            txt = 'Угадал!'
+            AI_number = get_random_int()
+        return render_template('index.html',ans = txt,old_value=user_input)
+
+    return render_template('index.html', ans = 'пуп')
     
