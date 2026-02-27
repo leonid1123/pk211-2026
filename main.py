73d9e6a7a134de68d9868267715f47a576dd92c7
Leo <leonid112@yandex.ru>
2026-02-27 10:34:53 +0300

    список продуктов

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-# flask --app main run
 from flask import Flask
 from flask import render_template
 from flask import request
-import random
 
 app = Flask(__name__)
 
-def get_random_int():
-    return random.randrange(0,10)
+list_of_goods = []
 
-AI_number = get_random_int()
-
-@app.route("/", methods=['POST','GET'])
+@app.route("/", methods=['POST', 'GET'])
 def main_page():
-    global AI_number
-    if request.method == 'POST':
-        user_input = int(request.form['num'])
-        if user_input > AI_number:
-            txt = 'Чиселка очень большая'
-        elif user_input < AI_number:
-            txt = 'Чиселка очень маленькая'
-        else:
-            txt = 'Угадал!'
-            AI_number = get_random_int()
-        return render_template('index.html',ans = txt,old_value=user_input)
+    info = request.form.get('user_input')
+    list_of_goods.append(info)
+    return render_template(
+        'index.html',
+        my_list=list_of_goods)
 
-    return render_template('index.html', ans = 'пуп')
-    
+
+if __name__ == '__main__':
+    app.run()
