997243c2751554c58699c423fdc6dc172f79b477
Leo <leonid112@yandex.ru>
2026-03-30 10:39:16 +0300

    всякое

--- dict_example.py
+++ dict_example.py
@@ -1,3 +1,23 @@
-x = {1:'ivanov', 2:'petrova'}
+from flask import Flask, render_template, request
 
-print(x)
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def main_page():
+    h = 0
+    fam = ''
+    msg='Фамилия'
+    if request.method == "POST":
+        fam = request.form.get('s_name')
+        if fam == '':
+            msg = 'CЮда фамилию напиши!!!'
+        x = request.form.get('mark1')
+        y = request.form.get('mark2')
+        z = request.form.get('mark3')
+        if x.isnumeric() and y.isnumeric() and z.isnumeric():
+            h = (int(x)+int(y)+int(z))/3
+    return render_template('test.html', result=h, fam=fam, msg=msg)
+
+
+app.run(debug=True)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import pymysql.cursors
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -12,17 +14,31 @@ conn = pymysql.connect(host='127.0.0.1',
                        cursorclass=pymysql.cursors.DictCursor)
 cur = conn.cursor()
 
+UPLOAD_FOLDER = 'static/ing'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route("/", methods=['POST', 'GET'])
 def main_page():
-    user = request.form.get('login')
-    login = request.form.get('password')
-    if 'user_login' in request.form.keys():
-        print('Вход пользователя')
-    if 'guest_login' in request.form.keys():
-        print('Вход гостя')
+    msg = ''
+    if request.method == "POST":
+        color = request.form.get('inp_color')
+        size = request.form.get('inp_size')
+        material = request.form.get("inp_material")
 
-    return render_template('index.html')
+        img = request.files['inp_img']
+        filename = secure_filename(img.filename)
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        sql = 'INSERT INTO t_shirt(color, size, material, img) VALUES(%s,%s,%s,%s)'
+        ans = cur.execute(sql,(color,size,material,filename))
+        conn.commit()
+        if ans == 1:
+            msg = 'Футболка добавлена'
+        else:
+            msg = 'Ошибка запроса'
+
+    return render_template('index.html', msg=msg)
 
 
 if __name__ == '__main__':
