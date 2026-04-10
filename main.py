c588b06cb5cee06a8789425255a32145a4cfb6e9
Leo <leonid112@yandex.ru>
2026-04-10 10:31:55 +0300

    всякое

--- main.py
+++ main.py
@@ -43,16 +43,32 @@ def main_page():
 
 @app.route("/show", methods=['POST', 'GET'])
 def show_all():
+    params = None
+    sql_filter = 'SELECT DISTINCT color from t_shirt'
+    cur.execute(sql_filter)
+    ans_filter = cur.fetchall()
     sql = 'SELECT * FROM t_shirt'
     if request.method == "POST":
         if 'sort_up' in request.form:
             sql = 'SELECT * FROM t_shirt ORDER BY size ASC'
         if 'sort_down' in request.form:
             sql = 'SELECT * FROM t_shirt ORDER BY size DESC'
+        if 's_filter' in request.form:
+            sql = 'SELECT * FROM t_shirt WHERE color=%s'
+            filter_input = request.form.get('filter')
+            params = filter_input
+        if 'search' in request.form:
+            search_input = request.form.get('search')
+            search_input = '%'+search_input+'%'
+            print('start:',search_input)
+            sql = "SELECT * FROM t_shirt WHERE material LIKE %s"
+            params = search_input
 
-    cur.execute(sql)
+    cur.execute(sql, params)
     ans = cur.fetchall()
-    return render_template('main_page.html',all_list=ans)
+    return render_template('main_page.html',
+                           all_list=ans,
+                           ans_filter=ans_filter)
 
 
 if __name__ == '__main__':
